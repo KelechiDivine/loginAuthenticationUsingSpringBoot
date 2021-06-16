7110af2865245157cbd99e5edaa735688326ce1c
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-16 05:00:05 +0100

    project just starting

---
+++ src/main/java/com/java/springSecurity/BasicSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.java.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BasicSecurityApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSecurityApplication.class, args);
+		
+		
+	}
+	
+}
---
+++ src/test/java/com/java/springSecurity/BasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
